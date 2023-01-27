1f02e9f8aeaddb2784e924484748149abb2307c2
Real4 <Real4@DESKTOP-QDSVSV8>
2023-01-27 20:54:48 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,402 @@
+﻿#pragma region 4
+//// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+////
+//#include <iomanip>;
+//#include <iostream>
+//
+//
+//using namespace std;
+//
+//int main()
+//{
+//    setlocale(LC_ALL, "ru");
+//    //Вывод пустой строки 
+//    cout << "\n";
+//    //Ввод числа
+//    int number;
+//    cin >> number;
+//    int number1 = number / 1000;
+//    int number2 = number / 100 % 10;
+//    int numer3 = number / 10 % 10;
+//    int number4 = number % 10;
+//
+//    //
+//        
+//    if (number == 1 
+//        || number == 21
+//        || number == 31
+//        || number == 41
+//        || number == 51
+//        || number == 61
+//        || number == 71
+//        || number == 81
+//        || number == 91)
+//    {
+//    cout << number << " корова";
+//    }
+//    else if (number == 2
+//        || number % 10 == 3
+//        || number == 4
+//        || number == 22
+//        || number == 23
+//        || number == 24
+//        || number == 32
+//        || number == 33
+//        || number == 34
+//        || number == 42
+//        || number == 43
+//        || number == 44
+//        || number == 52
+//        || number == 53
+//        || number == 54
+//        || number == 62
+//        || number == 63
+//        || number == 64
+//        || number == 72
+//        || number == 73
+//        || number == 74
+//        || number == 82
+//        || number == 83
+//        || number == 84
+//        || number == 92
+//        || number == 93
+//        || number == 94)
+//    {
+//        cout << number << " коровы";
+//    }
+//    else if (number == 5 
+//        || number == 6 
+//        || number == 7 
+//        || number == 8 
+//        || number == 9 
+//        || number == 10
+//        || number == 11
+//        || number == 12
+//        || number == 13
+//        || number == 14
+//        || number == 15 
+//        || number == 16
+//        || number == 17
+//        || number == 18
+//        || number == 19
+//        || number == 20
+//        || number == 25
+//        || number == 26
+//        || number == 27
+//        || number == 28
+//        || number == 29
+//        || number == 30
+//        || number == 35
+//        || number == 36
+//        || number == 37
+//        || number == 38
+//        || number == 39
+//        || number == 40
+//        || number == 45
+//        || number == 46
+//        || number == 47
+//        || number == 48
+//        || number == 49
+//        || number == 50
+//        || number == 55
+//        || number == 56
+//        || number == 57
+//        || number == 58
+//        || number == 59
+//        || number == 60
+//        || number == 65
+//        || number == 66
+//        || number == 67
+//        || number == 68
+//        || number == 69
+//        || number == 70
+//        || number == 75
+//        || number == 76
+//        || number == 77
+//        || number == 78
+//        || number == 79
+//        || number == 80
+//        || number == 85
+//        || number == 86
+//        || number == 87
+//        || number == 88
+//        || number == 89
+//        || number == 90
+//        || number == 95
+//        || number == 96
+//        || number == 97
+//        || number == 98
+//        || number == 99
+//        || number == 100)
+//    {
+//        cout << number << " коров";
+//}
+//  
+//}
+#pragma endregion
+//// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+//// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+//
+//// Советы по началу работы 
+////   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+////   2. В окне Team Explorer можно подключиться к системе управления версиями.
+////   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+#include <iomanip>;
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+#pragma region 5
+    ////Вывод пустой строки 
+    //cout << "\n";
+
+    ////Ввод перого числа
+    //int number1;
+    //cin >> number1;
+
+    ////Ввод второго числа
+    //int number2;
+    //cin >> number2;
+
+    ////Вод третьего числа
+    //int number3;
+    //cin >> number3;
+
+    //if (number1 > number2 && number1 > number3)
+    //{
+    //    if (number2 > number3)
+    //    {
+    //        cout << number1 << number2 << number3;
+    //    }
+    //    else {
+    //        cout << number1 << number3 << number2;
+    //    }
+    //}
+    //else if (number2 > number1 && number2 > number3)
+    //{
+    //    if (number1 > number3)
+    //    {
+    //        cout << number2 << number1 << number3;
+    //    }
+    //    else {
+    //        cout << number2 << number3 << number1;
+    //    }
+    //}
+    //else if (number3 > number1 && number3 > number2)
+    //{
+    //    if (number1 > number2)
+    //    {
+    //        cout << number3 << number1 << number2;
+    //    }
+    //    else {
+    //        cout << number3 << number2 << number1;
+    //    }
+
+    //}
+#pragma endregion
+
+#pragma region 6
+
+
+   ////Вывод пустой строки 
+   //cout << "\n";
+
+   ////Ввод числа
+   // int number;
+   // cin >> number;
+   // while (number > 0)
+   // {
+   //     cout << number % 10 << endl;
+   //     number = number / 10;
+   // }
+#pragma endregion
+
+//    int numer3 = number / 10 % 10;
+//    int number4 = number % 10;
+#pragma endregion
+
+#pragma region 7
+//    //Вывод пустой строки 
+//    cout << "\n";
+//
+//    //Ввод числа
+//    int a;
+//    cin >> a;
+//    for (int i = 1; i <= 9; i++) {
+//        if (a % i == 0)
+//        {
+//            cout << i;
+//        }
+//    }
+//}
+#pragma endregion 
+
+#pragma region 8
+//    //Вывод пустой строки 
+//    cout << "\n";
+//    // Ввод числа
+//    int number, sum = 0;
+//    cin >> number;
+//    while (number != 0) {
+//        int cfra = number % 10;
+//        cout << sum << "+" << cfra << endl;
+//        sum = sum + cfra;
+//        number = number / 10;
+//    }
+//}
+#pragma endregion
+
+#pragma region 9
+
+//    //Вывод пустой строки 
+//    cout << "\n";
+//    // Ввод числа
+//    int number;
+//    int num;
+//    cout << "номер";
+//    cin >> number;
+//    num = 0;
+//    while (number != 0) {
+//        int cifra = number % 10;
+//        number = number / 10;
+//        cout << cifra;
+//    }
+//    return 0;
+//}
+#pragma endregion
+
+#pragma region 10
+//     // Вывод пустой строки
+//    cout << "\n";
+//    // Ввод числа
+//    int number, error = 0;
+//    cout << "Пожалуйста, введите число от 1 до 5 \n";    
+//    cin >> number;
+//    int r = rand() % (5)+1;
+//
+//    while (number != r)
+//    {
+//
+//        if (number < r)
+//        {
+//            cout << "Не угадали! Загаданное число больше! \n";
+//            cin >> number;
+//            error++;
+//        }
+//        else if (number > r) {
+//            cout << "Не угадали! Загаданное число меньше! \n";
+//            cin >> number;
+//            error++;
+//        }
+//        
+//        if (error >= 5) {
+//            cout << "Увы! Игра закончена! Вы использовали все попытки! \n";
+//            exit(0);
+//        }
+//    }
+//} 
+#pragma endregion
+
+#pragma region 11
+//    // Вывод пустой строки
+//    cout << "\n";
+//    // Ввод числа
+//    int firstNumber, lastNumber;
+//    int sum = 0;
+//    cin >> firstNumber;
+//    cin >> lastNumber;
+//        
+//    for (int i = firstNumber; i <= lastNumber; i++)
+//    {
+//            sum = sum + i;
+//            cout << sum << endl;
+//    }
+//
+//}
+
+#pragma endregion
+#pragma region 12
+
+//    //Вывод пустой строки
+//    cout << "\n";
+//    cout << "Пожалуйста, введите любое число\n";
+//    // Ввод числа
+//    int number;
+//    int sum = 0;
+//    cin >> number;
+//    
+//    while (number != 0) {
+//        sum = sum + number;
+//        cin >> number;
+//    } 
+//    cout << sum << endl;
+//}
+
+#pragma endregion
+#pragma region 13
+//    // Вывод пустой строки
+//    cout << "\n";
+//    cout << "Пожалуйста, введите любые границы диапазона\n";
+//    // Ввод числа
+//    int firstNumber, lastNumber, temp; 
+//    cin >> firstNumber;
+//    cin >> lastNumber;
+//
+//    if (firstNumber > lastNumber)
+//    {
+//        temp = lastNumber;
+//        lastNumber = firstNumber;
+//        firstNumber = temp;
+//    }
+//
+//    for (int i = firstNumber; i <= lastNumber; i++)
+//    {
+//        if (i % 2 == 0 && i % 7 == 0) {
+//            cout << i << endl;
+//        }
+//
+//    }
+//
+//}
+#pragma endregion
+
+#pragma region 14
+
+  /*  int number1 = 2 + (rand() % (10 - 2 + 1));
+      int number2 = 2 + (rand() % (10 - 2 + 1));
+    
+
+    cout << "Пожалуйста, умножьте  " << number1 << " на " << number2 << "\n";
+
+    int number3;
+    cin >> number3;
+    if (number1 * number2 == number3) {
+
+        cout << "Ответ верный!\n";
+        }
+    else {
+
+        cout << "Ответ неверный! \n";
+    }
+
+    }*/
+#pragma endregion
+
+#pragma region 15
+
+string tree = "Tree";
+int price_tree = 30;
+int q_tree;
+
+
+
+int garland, сhristmasТreeballs, clapperboard, confetti, pricegarland, priceсhristmasТreeballs, price;
+
